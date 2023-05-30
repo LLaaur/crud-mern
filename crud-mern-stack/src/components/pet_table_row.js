3eb439e4a5cbf9eb24bedd4508d30313b9ae71e0
Laur <laur_laurentiu2013@yahoo.com>
2023-05-30 18:07:29 +0300

    create pet table component

--- crud-mern-stack/src/components/pet_table_row.js
+++ crud-mern-stack/src/components/pet_table_row.js
@@ -1,8 +1,49 @@
-
+import React from "react";
+import {Button} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import axios from "axios";
 
 
 const PetTableRow = () => {
 
-}
+    const {_id, name, species, breed, age, location} = props.obj;
+
+    const deletePet = () => {
+        axios.delete(
+            "http://localhost:4000/pets/delete_pet" + _id
+        )
+            .then((res) => {
+                if(res.status === 200){
+                    alert("Pet successfully deleted.");
+                    window.location.reload();
+                }
+                else {
+                    Promise.reject();
+                }
+            })
+            .catch((err) => {
+                alert("Something went wrong, please try again");
+            })
+    }
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{species}</td>
+            <td>{breed}</td>
+            <td>{age}</td>
+            <td>{location}</td>
+            <td>
+                <Link className="edit-link" to={"/edit_pet_info/" + _id}>
+                    Edit
+                </Link>
+                <Button onClick={deletePet} size="sm" variant="danger">
+                    Delete
+                </Button>
+            </td>
+        </tr>
+    );
+
+};
 
 export default PetTableRow;
